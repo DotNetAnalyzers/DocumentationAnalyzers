5e0e9bef8f43629bbba103875aa86bce948ef370
Sam Harwell <sam@tunnelvisionlabs.com>
2018-09-17 01:13:30 -0500

    Implement DOC103 (Use XML Documentation Syntax)
    Closes #2
    Closes #3
    Closes #4

---
+++ DocumentationAnalyzers/DocumentationAnalyzers.CodeFixes/StyleRules/DOC103CodeFixProvider.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT license. See LICENSE in the project root for license information.
+
+namespace DocumentationAnalyzers.StyleRules
+{
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using DocumentationAnalyzers.Helpers;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeActions;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(DOC103CodeFixProvider))]
+    [Shared]
+    internal class DOC103CodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds { get; }
+            = ImmutableArray.Create(DOC103UseXmlDocumentationSyntax.DiagnosticId);
+
+        public override FixAllProvider GetFixAllProvider()
+            => CustomFixAllProviders.BatchFixer;
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                if (!FixableDiagnosticIds.Contains(diagnostic.Id))
+                {
+                    continue;
+                }
+
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        StyleResources.DOC103CodeFix,
+                        token => GetTransformedDocumentAsync(context.Document, diagnostic, token),
+                        nameof(DOC103CodeFixProvider)),
+                    diagnostic);
+            }
+
+            return SpecializedTasks.CompletedTask;
+        }
+
+        private static async Task<Document> GetTransformedDocumentAsync(Document document, Diagnostic diagnostic, CancellationToken cancellationToken)
+        {
+            SyntaxNode root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            SyntaxToken token = root.FindToken(diagnostic.Location.SourceSpan.Start, findInsideTrivia: true);
+
+            var xmlElement = token.Parent.FirstAncestorOrSelf<XmlElementSyntax>();
+            var oldStartToken = xmlElement.StartTag.Name.LocalName;
+
+            string newIdentifier;
+            switch (oldStartToken.ValueText)
+            {
+            case "p":
+                newIdentifier = XmlCommentHelper.ParaXmlTag;
+                break;
+
+            case "tt":
+                newIdentifier = XmlCommentHelper.CXmlTag;
+                break;
+
+            case "pre":
+                newIdentifier = XmlCommentHelper.CodeXmlTag;
+                break;
+
+            case "ul":
+            case "ol":
+                newIdentifier = XmlCommentHelper.ListXmlTag;
+                break;
+
+            default:
+                // Not handled
+                return document;
+            }
+
+            var newStartToken = SyntaxFactory.Identifier(oldStartToken.LeadingTrivia, newIdentifier, oldStartToken.TrailingTrivia);
+            var newXmlElement = xmlElement.ReplaceToken(oldStartToken, newStartToken);
+
+            var oldEndToken = newXmlElement.EndTag.Name.LocalName;
+            var newEndToken = SyntaxFactory.Identifier(oldEndToken.LeadingTrivia, newIdentifier, oldEndToken.TrailingTrivia);
+            newXmlElement = newXmlElement.ReplaceToken(oldEndToken, newEndToken);
+
+            if (newIdentifier == XmlCommentHelper.ListXmlTag)
+            {
+                // Add an attribute for the list kind
+                string listType = oldStartToken.ValueText == "ol" ? "number" : "bullet";
+                newXmlElement = newXmlElement.WithStartTag(newXmlElement.StartTag.AddAttributes(XmlSyntaxFactory.TextAttribute(XmlCommentHelper.TypeAttributeName, listType)));
+
+                // Replace each <li>...</li> element with <item><description>...</description></item>
+                for (int i = 0; i < newXmlElement.Content.Count; i++)
+                {
+                    if (newXmlElement.Content[i] is XmlElementSyntax childXmlElement
+                        && childXmlElement.StartTag?.Name?.LocalName.ValueText == "li"
+                        && childXmlElement.StartTag.Name.Prefix == null)
+                    {
+                        oldStartToken = childXmlElement.StartTag.Name.LocalName;
+                        newStartToken = SyntaxFactory.Identifier(oldStartToken.LeadingTrivia, XmlCommentHelper.ItemXmlTag, oldStartToken.TrailingTrivia);
+                        var newChildXmlElement = childXmlElement.ReplaceToken(oldStartToken, newStartToken);
+
+                        oldEndToken = newChildXmlElement.EndTag.Name.LocalName;
+                        newEndToken = SyntaxFactory.Identifier(oldEndToken.LeadingTrivia, XmlCommentHelper.ItemXmlTag, oldEndToken.TrailingTrivia);
+                        newChildXmlElement = newChildXmlElement.ReplaceToken(oldEndToken, newEndToken);
+
+                        newChildXmlElement = newChildXmlElement.WithContent(XmlSyntaxFactory.List(XmlSyntaxFactory.Element(XmlCommentHelper.DescriptionXmlTag, newChildXmlElement.Content)));
+
+                        newXmlElement = newXmlElement.ReplaceNode(childXmlElement, newChildXmlElement);
+                    }
+                }
+            }
+
+            return document.WithSyntaxRoot(root.ReplaceNode(xmlElement, newXmlElement));
+        }
+    }
+}
---
+++ DocumentationAnalyzers/DocumentationAnalyzers/StyleRules/DOC103UseXmlDocumentationSyntax.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT license. See LICENSE in the project root for license information.
+
+namespace DocumentationAnalyzers.StyleRules
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    /// <summary>
+    /// Use XML documentation syntax.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class DOC103UseXmlDocumentationSyntax : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// The ID for diagnostics produced by the <see cref="DOC103UseXmlDocumentationSyntax"/> analyzer.
+        /// </summary>
+        public const string DiagnosticId = "DOC103";
+        private const string HelpLink = "https://github.com/DotNetAnalyzers/DocumentationAnalyzers/blob/master/docs/DOC103.md";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(StyleResources.DOC103Title), StyleResources.ResourceManager, typeof(StyleResources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(StyleResources.DOC103MessageFormat), StyleResources.ResourceManager, typeof(StyleResources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(StyleResources.DOC103Description), StyleResources.ResourceManager, typeof(StyleResources));
+
+        private static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, AnalyzerCategory.StyleRules, DiagnosticSeverity.Info, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+            = ImmutableArray.Create(Descriptor);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(HandleXmlElementSyntax, SyntaxKind.XmlElement);
+        }
+
+        private static void HandleXmlElementSyntax(SyntaxNodeAnalysisContext context)
+        {
+            var xmlElementSyntax = (XmlElementSyntax)context.Node;
+            var name = xmlElementSyntax.StartTag?.Name;
+            if (name is null || name.Prefix != null)
+            {
+                return;
+            }
+
+            switch (name.LocalName.ValueText)
+            {
+            case "p":
+            case "pre":
+            case "tt":
+            case "ol":
+            case "ul":
+                break;
+
+            default:
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, name.LocalName.GetLocation()));
+        }
+    }
+}
