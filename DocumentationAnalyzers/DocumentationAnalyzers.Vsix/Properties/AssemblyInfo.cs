648a504b40126091f336c408880634838c1962ad
Sam Harwell <sam@tunnelvisionlabs.com>
2018-09-20 11:41:24 -0500

    Make sure the VSIX output includes CommonMark

---
+++ DocumentationAnalyzers/DocumentationAnalyzers.Vsix/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT license. See LICENSE in the project root for license information.
+
+using Microsoft.VisualStudio.Shell;
+
+[assembly: ProvideCodeBase(CodeBase = "CommonMark.dll")]
