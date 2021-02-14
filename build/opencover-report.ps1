f051d07208a14553b8d5d6833063818879329733
Sam Harwell <sam@tunnelvisionlabs.com>
2021-02-14 12:52:46 -0800

    Update xunit to 2.4.1

--- build/opencover-report.ps1
+++ build/opencover-report.ps1
@@ -33,7 +33,8 @@ $pdb2pdb_version = $packageConfig.SelectSingleNode('/packages/package[@id="Micro
 
 $packages_folder = '..\packages'
 $opencover_console = "$packages_folder\OpenCover.$opencover_version\tools\OpenCover.Console.exe"
-$xunit_runner_console = "$packages_folder\xunit.runner.console.$xunitrunner_version\tools\xunit.console.x86.exe"
+$xunit_runner_console_net452 = "$packages_folder\xunit.runner.console.$xunitrunner_version\tools\net452\xunit.console.x86.exe"
+$xunit_runner_console_net46 = "$packages_folder\xunit.runner.console.$xunitrunner_version\tools\net46\xunit.console.x86.exe"
 $report_generator = "$packages_folder\ReportGenerator.$reportgenerator_version\tools\ReportGenerator.exe"
 $pdb2pdb = "$packages_folder\Microsoft.DiaSymReader.Pdb2Pdb.$pdb2pdb_version\tools\Pdb2Pdb.exe"
 $report_folder = '.\OpenCover.Reports'
@@ -117,7 +118,7 @@ If ($AppVeyor) {
 	-excludebyfile:*\*Designer.cs `
 	-searchdirs:"$symbols_folder_csharp6" `
 	-output:"$report_folder\OpenCover.DocumentationAnalyzers.xml" `
-	-target:"$xunit_runner_console" `
+	-target:"$xunit_runner_console_net452" `
 	-targetargs:"$target_dll -noshadow $AppVeyorArg"
 
 If ($AppVeyor -and -not $?) {
@@ -135,7 +136,7 @@ If ($AppVeyor -and -not $?) {
 	-searchdirs:"$symbols_folder_csharp7" `
 	-output:"$report_folder\OpenCover.DocumentationAnalyzers.xml" `
 	-mergebyhash -mergeoutput `
-	-target:"$xunit_runner_console" `
+	-target:"$xunit_runner_console_net46" `
 	-targetargs:"$target_dll_csharp7 -noshadow $AppVeyorArg"
 
 If ($AppVeyor -and -not $?) {
